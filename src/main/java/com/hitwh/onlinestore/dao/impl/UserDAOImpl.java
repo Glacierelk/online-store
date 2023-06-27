d4744e5db1c13b7ce2a8b3245bcc99d47a27f36f
kurisu0306 <zh13587168037@gmail.com>
2023-06-27 16:43:11 +0800

    UserDAO

---
+++ src/main/java/com/hitwh/onlinestore/dao/impl/UserDAOImpl.java
@@ -0,0 +1,82 @@
+package com.hitwh.onlinestore.dao.impl;
+
+import com.hitwh.onlinestore.dao.UserDAO;
+import com.hitwh.onlinestore.pojo.User;
+import com.hitwh.onlinestore.utils.JDBCUtils;
+import org.junit.Test;
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserDAOImpl implements UserDAO {
+    private JdbcTemplate template = new JdbcTemplate(JDBCUtils.getDataSource());
+
+    @Override
+    public void addUser(User user) {
+        String sql = "insert into user(name, password, email, address) values(?, ?, ?, ?)";
+        String name = user.getName();
+        String password = user.getPassword();
+        String email = user.getEmail();
+        String address = user.getAddress();
+        template.update(sql, name, password, email, address);
+    }
+
+    @Override
+    public User getUserByName(String name) {
+        String sql = "select id,name,password,email,address from user where name = ?";
+        try {
+            return template.queryForObject(sql, new BeanPropertyRowMapper<User>(User.class), name);
+        } catch (DataAccessException e) {
+            return null;
+        }
+    }
+
+    @Override
+    public User getUserById(int id) {
+        String sql = "select id,name,password,email,address from user where id = ?";
+        try {
+            return template.queryForObject(sql, new BeanPropertyRowMapper<User>(User.class), id);
+        } catch (DataAccessException e) {
+            return null;
+        }
+    }
+
+    @Override
+    public User getUserByNameAndPassword(String name, String password) {
+        String sql = "select * from user where name = ? and password = ?";
+        try {
+            return template.queryForObject(sql, new BeanPropertyRowMapper<User>(User.class), name, password);
+        } catch (DataAccessException e) {
+            return null;
+        }
+
+    }
+
+    @Override
+    public boolean deleteUserByName(String name) {
+        String sql = "delete from user where name = ?";
+        int count = template.update(sql, name);
+        return count != 0;
+    }
+
+    @Override
+    public int getTotal() {
+        String sql = "select count(id) from user";
+        return template.queryForObject(sql, Integer.class);
+    }
+
+    @Override
+    public List<User> getUserList(int start, int count) {
+        String sql = "select * from user limit ?, ?";
+        return template.query(sql, new BeanPropertyRowMapper<User>(User.class), start, count);
+    }
+
+    @Test
+    public void test1() {
+        deleteUserByName("test");
+    }
+}
+
---
+++ src/main/java/com/hitwh/onlinestore/pojo/User.java
@@ -0,0 +1,62 @@
+package com.hitwh.onlinestore.pojo;
+
+public class User {
+    private int id;
+    private String name;
+    private String password;
+    private String email;
+    private String address;
+
+
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", password='" + password + '\'' +
+                ", email='" + email + '\'' +
+                ", address='" + address + '\'' +
+                '}';
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
