2ccffa9d1168409ee3f7f6ef874c02eede85215a
kurisu0306 <zh13587168037@gmail.com>
2023-06-28 08:27:56 +0800

    UserDAO

---
+++ src/main/java/com/hitwh/onlinestore/web/UserServlet.java
@@ -0,0 +1,11 @@
+package com.hitwh.onlinestore.web;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+import java.io.IOException;
+
+@WebServlet("/Servlet")
+public class UserServlet extends BaseServlet {
+
+}
