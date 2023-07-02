da951919cf8858802507c340fc86b762ced541db
Glacierelk <2191710503@qq.com>
2023-07-02 10:25:37 +0800

    202307021022

--- frontend/src/components/user-facing/ShoppingCartComponent.vue
+++ frontend/src/components/user-facing/ShoppingCartComponent.vue
@@ -13,7 +13,8 @@ export default {
     return {
       show: false,
       tableData: [],
-      selection: [],
+      summary: 0,
+      total: 0
     }
   },
   methods: {
@@ -54,22 +55,20 @@ export default {
     },
     handleSelectionChange(selection){
       console.log(selection);
-      this.selection = selection;
-    },
-    getSum() {
-      let sums = ["总计", "", "", "", "", "", ""];
       let sum = 0;
-      this.selection.forEach(item => {
+      let cnt = 0;
+      selection.forEach(item => {
         sum += item.count * item.product.promotePrice;
+        cnt += item.count;
       })
-      sums[5] = "￥ " + String(sum.toFixed(2));
-      return sums;
-    },
-    isSummaryRow(scope) {
-      return scope.row.id === this.tableData.length + 1;
+      this.summary = sum.toFixed(2);
+      this.total = cnt;
     },
     settlement() {
       alert("jiesuan")
+    },
+    deleteCarts() {
+      alert("deleteCarts")
     }
   },
   async mounted() {
@@ -95,55 +94,64 @@ export default {
 </script>
 
 <template>
-  <el-table
-      v-if="show"
-      :data="tableData"
-      class="cart-show"
-      stripe
-      :header-cell-style="{'text-align':'center'}"
-      @selection-change="handleSelectionChange"
-      show-summary
-      :summary-method="getSum"
-      width="100%"
-  >
-    <el-table-column type="selection" width="80" align="center" fixed="left" />
 
-    <el-table-column label="商品图片" prop="imageId" align="center" width="100">
-      <template v-slot="scope">
-        <el-image :src="getImagePath(scope.row.imageId)"></el-image>
-      </template>
-    </el-table-column>
+  <div>
+    <el-table
+        v-if="show"
+        :data="tableData"
+        class="cart-show"
+        stripe
+        :header-cell-style="{'text-align':'center'}"
+        @selection-change="handleSelectionChange"
+        width="100%"
+    >
+      <el-table-column type="selection" width="80" align="center" fixed="left" />
 
-    <el-table-column label="商品信息" prop="product.name" align="left" width="800"/>
+      <el-table-column label="商品图片" prop="imageId" align="center" width="100">
+        <template v-slot="scope">
+          <el-image :src="getImagePath(scope.row.imageId)"></el-image>
+        </template>
+      </el-table-column>
 
-    <el-table-column label="单价" prop="product.promotePrice" align="center" width="200">
-      <template v-slot="scope">
-        <span class="price-show">￥&nbsp;{{scope.row.product.promotePrice}}</span>
-      </template>
-    </el-table-column>
+      <el-table-column label="商品信息" prop="product.name" align="left" width="800"/>
 
-    <el-table-column label="数量" prop="count" align="center" width="250">
-      <template v-slot="scope">
-        <el-input-number v-model="scope.row.count" :max="scope.row.product.stock" :min="1"
-                         @change="changeCount(scope.row.id, scope.row.count)"></el-input-number>
-      </template>
-    </el-table-column>
+      <el-table-column label="单价" prop="product.promotePrice" align="center" width="200">
+        <template v-slot="scope">
+          <span class="price-show">￥&nbsp;{{scope.row.product.promotePrice}}</span>
+        </template>
+      </el-table-column>
 
-    <el-table-column label="总价" align="center" width="150" fixed="right">
-      <template v-slot="scope">
-        <span class="price-show">￥&nbsp;{{scope.row.product.promotePrice * scope.row.count}}</span>
-      </template>
-    </el-table-column>
+      <el-table-column label="数量" prop="count" align="center" width="250">
+        <template v-slot="scope">
+          <el-input-number v-model="scope.row.count" :max="scope.row.product.stock" :min="1"
+                           @change="changeCount(scope.row.id, scope.row.count)"></el-input-number>
+        </template>
+      </el-table-column>
 
-    <el-table-column label="操作" fixed="right" align="center" width="100">
-      <template v-slot="scope">
-        <el-button v-if="isSummaryRow(scope)" type="danger" @click="settlement">结算</el-button>
-        <el-button v-else type="danger" @click="deleteCart(scope.row.id)" plain>删除</el-button>
-      </template>
-    </el-table-column>
+      <el-table-column label="总价" align="center" width="150" fixed="right">
+        <template v-slot="scope">
+          <span class="price-show">￥&nbsp;{{scope.row.product.promotePrice * scope.row.count}}</span>
+        </template>
+      </el-table-column>
 
-  </el-table>
+      <el-table-column label="操作" fixed="right" align="center" width="150">
+        <template #header>
+          <el-button type="danger" @click="deleteCarts" plain>批量删除</el-button>
+        </template>
+        <template v-slot="scope">
+          <el-button type="danger" @click="deleteCart(scope.row.id)" plain>删除</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
 
+    <div id="summary-row" v-if="show">
+      <span style="vertical-align: center">已选商品&nbsp;</span>
+      <span class="price-show">{{ this.total }}</span>
+      <span style="vertical-align: center">&nbsp;件，总价&nbsp;</span>
+      <span class="price-show" style="font-size: 23px; font-weight: bold">￥&nbsp;{{ this.summary }}</span>
+      <el-button type="danger" @click="settlement" id="settlement-button">结算</el-button>
+    </div>
+  </div>
 
 </template>
 
@@ -155,6 +163,26 @@ export default {
 
 .el-table_6_column_41 {
   color: #C60A0A;
+}
+
+#summary-row {
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  background-color: #e5e5e5;
+  height: 60px;
+  margin-top: 20px;
+  border-radius: 10px;
+  vertical-align: center;
+  text-align: center;
+}
+
+#settlement-button {
+  font-size: 16px;
+  font-weight: bold;
+  padding: 18px 26px;
+  margin-left: 30px;
+  margin-right: 30px;
 }
 
 </style>
